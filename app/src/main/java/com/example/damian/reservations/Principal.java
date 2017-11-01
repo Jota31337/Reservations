c5efd4e2f12829eed250e88106a7d49b4427b35a
damianm <damian3095007@gmail.com>
2017-10-31 19:59:49 -0500

    Vista Login Diseñando

--- app/src/main/java/com/example/damian/reservations/Principal.java
+++ app/src/main/java/com/example/damian/reservations/Principal.java
@@ -1,5 +1,6 @@
 package com.example.damian.reservations;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,7 @@ public class Principal extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(Principal.this,Login.class);startActivity(i);
             }
         });
 
