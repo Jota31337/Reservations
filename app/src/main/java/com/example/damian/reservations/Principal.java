19dfee6179007e233236ef4cbdb2c20bb7d451b2
damian <damian9530007@gmail.com>
2017-11-02 12:47:33 -0500

    Menus

--- app/src/main/java/com/example/damian/reservations/Principal.java
+++ app/src/main/java/com/example/damian/reservations/Principal.java
@@ -24,13 +24,7 @@ public class Principal extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
