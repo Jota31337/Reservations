b5de569276d71f8c846b9791a9dac9d97aadebff
damian <damian9530007@gmail.com>
2017-11-08 15:42:00 -0500

    diseño modificado

--- app/src/main/java/com/example/damian/reservations/AdaptadorCanchas.java
+++ app/src/main/java/com/example/damian/reservations/AdaptadorCanchas.java
@@ -37,7 +37,7 @@ public class AdaptadorCanchas extends RecyclerView.Adapter<AdaptadorCanchas.Canc
     public void onBindViewHolder(CanchasViewHolder holder, int position) {
 
         final Cancha_Establecimiento p = canchas_res.get(position);
-        holder.foto.setImageDrawable(ResourcesCompat.getDrawable(res,R.drawable.user,null));
+        holder.foto.setImageDrawable(ResourcesCompat.getDrawable(res,R.drawable.fondo,null));
         holder.tamano.setText(p.getTamano());
         holder.direccion.setText(p.getDireccion());
         holder.celular.setText(p.getCelular()+"");
